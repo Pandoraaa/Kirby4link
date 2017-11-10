dc66007f9a28227a3c70b7c987ea158ca3db460f
Pandora <pandoraangora@gmail.com>
2017-11-10 14:48:49 +0100

    merci florian

--- src/Model/Match.php
+++ src/Model/Match.php
@@ -1,43 +1,41 @@
 <?php
-namespace Kirby4link\Model;
-require '../../vendor/autoload.php';
 
+define("DSN", "mysql:host=localhost;dbname=link");
+define("USER", "root");
+define("PASS", "9f8a7fc4b89ec2ecc8ab458a7f30f0c00471be5b8555eeb9");
 
+$db = new \PDO(DSN,USER,PASS);
 
+$req = $db->query("SELECT pseudo FROM user WHERE MONTH(date) = MONTH(NOW()) AND DAY(date) = DAY(NOW())");
+$results = $req->fetchAll(\PDO::FETCH_CLASS) ;
 
-class Match extends Manager
-{
-    public function matchDate(){
+if (!empty($req)){
 
-        $req=$this->db->query("SELECT pseudo FROM user WHERE MONTH(date) = MONTH(NOW()) AND DAY(date) = DAY(NOW())");
-        $result=$req->fetchAll() ;
+    $client = new \GuzzleHttp\Client();
 
-        if (!empty($req)){
+    $login = $client->request('POST', 'https://chat.wildcodeschool.fr/api/v1/login', [
+        "form_params" => [
+            "user" => "cindy.liwenge@gmail.com",
+            "password" => "Cin0nym*27!"
+        ]
+    ]);
 
-            $client = new \GuzzleHttp\Client();
+    $auth = json_decode($login->getBody());
 
-            $login = $client->request('POST', 'https://chat.wildcodeschool.fr/api/v1/login', [
-                "form_params" => [
-                    "user" => "cindy.liwenge@gmail.com",
-                    "password" => "Cin0nym*27!"
-                ]
-            ]);
-
-            $auth = json_decode($login->getBody());
-
-            $send = $client->request('POST', 'https://chat.wildcodeschool.fr/api/v1/chat.postMessage', [
-                "headers" => [
-                    "X-Auth-Token" => $auth->data->authToken,
-                    "X-User-Id" => $auth->data->userId
-                ],
-                "form_params" => [
-                    "channel" => "#paris",
-                    "alias" => "Party Bot",
-                    "emoji" => ":beers:",
-                    "text" => "Heyyyyy c'est l'anniversaire de @Caroline! 
-        Qui est chaud pour boire au Bistrot des Artistes à 18h30 ce soir? Réagissez avec une choppe :beer:
-        https://media.giphy.com/media/3o7btZjaYxqkGyOYA8/giphy.gif "
-                ]
-            ]);
-        }
-    }}
+    foreach ($results as $result){
+        $client->request('POST', 'https://chat.wildcodeschool.fr/api/v1/chat.postMessage', [
+            "headers" => [
+                "X-Auth-Token" => $auth->data->authToken,
+                "X-User-Id" => $auth->data->userId
+            ],
+            "form_params" => [
+                "channel" => "#paris",
+                "alias" => "Party Bot",
+                "emoji" => ":beers:",
+                "text" => "Heyyyyy c'est l'anniversaire de " . $result->pseudo . " 
+Qui est chaud pour boire au Bistrot des Artistes à 18h30 ce soir? Réagissez avec une choppe :beer:
+https://media.giphy.com/media/3o7btZjaYxqkGyOYA8/giphy.gif "
+            ]
+        ]);
+    }
+}

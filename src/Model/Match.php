0a6c0a7b61d19f395ea7d12217cc3e1d001c70da
Pandora <pandoraangora@gmail.com>
2017-11-10 14:06:38 +0100

    match

--- src/Model/Match.php
+++ src/Model/Match.php
@@ -1,30 +1,42 @@
 <?php
+namespace Kirby4link\Model;
+require '../../vendor/autoload.php';
 
-require '../vendor/autoload.php';
 
-$client = new GuzzleHttp\Client();
 
-$login = $client->request('POST', 'https://chat.wildcodeschool.fr/api/v1/login', [
-    "form_params" => [
-        "user" =>  "cindy.liwenge@gmail.com",
-        "password" => "Cin0nym*27!"
-    ]
-]);
 
-$auth = json_decode($login->getBody());
+class Match extends Manager
+{
+    public function matchDate(){
+        $now = date('m-d');
+        $req=$this->db->query("SELECT pseudo FROM user WHERE date=$now");
+        $req ;
+        if (!empty($req)){
 
-$send = $client->request('POST', 'https://chat.wildcodeschool.fr/api/v1/chat.postMessage', [
-    "headers" => [
-        "X-Auth-Token"=> $auth->data->authToken,
-        "X-User-Id"=> $auth->data->userId
-    ],
-    "form_params" => [
-        "channel" => "#paris",
-        "alias" => "Party Bot",
-        "emoji" => ":beers:",
-        "text" => "Heyyyyy c'est l'anniversaire de @Caroline! 
+            $client = new \GuzzleHttp\Client();
+
+            $login = $client->request('POST', 'https://chat.wildcodeschool.fr/api/v1/login', [
+                "form_params" => [
+                    "user" => "cindy.liwenge@gmail.com",
+                    "password" => "Cin0nym*27!"
+                ]
+            ]);
+
+            $auth = json_decode($login->getBody());
+
+            $send = $client->request('POST', 'https://chat.wildcodeschool.fr/api/v1/chat.postMessage', [
+                "headers" => [
+                    "X-Auth-Token" => $auth->data->authToken,
+                    "X-User-Id" => $auth->data->userId
+                ],
+                "form_params" => [
+                    "channel" => "#paris",
+                    "alias" => "Party Bot",
+                    "emoji" => ":beers:",
+                    "text" => "Heyyyyy c'est l'anniversaire de @Caroline! 
         Qui est chaud pour boire au Bistrot des Artistes à 18h30 ce soir? Réagissez avec une choppe :beer:
         https://media.giphy.com/media/3o7btZjaYxqkGyOYA8/giphy.gif "
-    ]
-]);
-var_dump($send);die();
+                ]
+            ]);
+        }
+    }}
